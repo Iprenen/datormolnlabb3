e6392a4a9de17e24e8c63d35e0cd4fec2e62c038
Eric Stenberg <eric.stenberg@telia.com>
2015-10-14 14:51:32 +0200

    updated tasks.py

--- run.py
+++ run.py
@@ -1,4 +1,67 @@
-from tasks import wordcount
+#!flask/bin/python
+
+from celery import Celery
+from celery import group
+from tasks import parseTweets
+from flask import Flask, jsonify
+import subprocess
+import sys
+import os
+import swiftclient.client
+import json
+import time
+import urllib2
+from collections import Counter
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET'])
+def print_hello():
+	return 'Tjo Valle! Allt bra? :)', 200
+
+@app.route('/Labb3/messaging', methods=['GET'])
+def cow_say():
+	tweets = []
+	req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets")
+	response = urllib2.urlopen(req)
+	tweetsObject = response.read().split()
+	for t in tweetsObject:
+		tweets.append(t)
+
+	A = tweets[:4]
+	B = tweets[4:8]
+	C = tweets[8:12]
+	D = tweets[12:16]
+	E = tweets[16:]
+
+	job = group(parseTweets.s(A), 
+		parseTweets.s(B), 
+		parseTweets.s(C),
+		parseTweets.s(D),
+		parseTweets.s(E))
+
+	tweetTask = job.apply_async()
+	print "Celery is working..."
+	counter = 0
+	while (tweetTask.ready() == False):
+		print "... %i s" %(counter)
+		counter += 5
+		time.sleep(5)
+	print "The task is done!"
+
+	toReturn = tweetTask.get()
+
+	c = Counter()
+	for d in toReturn:
+		c.update(d)
+
+	return jsonify(dict(c)), 200
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',debug=True)
+
+
+'''from tasks import wordcount
 
 compresult = [0,0,0,0,0,0,0,0,0]
 
@@ -84,3 +147,5 @@ print "Antalet forekomster av ordet denne: " + str(compresult[5])
 print "Antalet forekomster av ordet hen: " + str(compresult[6])
 print "Antal tweets: " + str(compresult[7])
 print "Antal retweets: " + str(compresult[8])
+
+'''
