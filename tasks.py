8f3f3e6941a18a911a569b0f449b12ffa83360f1
Eric Stenberg <eric.stenberg@telia.com>
2015-10-14 14:54:12 +0200

    updated tasks.py

--- tasks.py
+++ tasks.py
@@ -7,39 +7,39 @@ app = Celery('tasks', backend='amqp', broker='amqp://worker:worker@192.168.0.5/r
 
 @app.task
 def wordcount(adress):
-
+    
     pronoms={"han": 0, "hon": 0, "den": 0, "det": 0, "denna": 0, "denne": 0, "hen": 0}
-	n = 0 
-
-	#tweets = conn.get_container("tweets")[1]
-	#tweets = tweets[0:1]
-	#tweets = [{"name":"tweets_19.txt"}]
-	for t in tweetName:
-		print t
-		req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + t)
-		response = urllib2.urlopen(req)
-		obj = response.read()
-		objects = open(t, 'w')
-		objects.write(obj)
-		objects.close()
-		objects = open(t, 'r')
-		for line in objects:
-			try:
-				jL = json.loads(line)
-				tmp1 = jL["text"].lower()
-				tmp = tmp1.split()
-				retweet = jL["retweet_count"]
-				if retweet == 0:
-					n += 1
-					for i in pronoms.keys():
-						if(i in tmp):
-							pronoms[i] += 1
+    n = 0 
+    
+    #tweets = conn.get_container("tweets")[1]
+    #tweets = tweets[0:1]
+    #tweets = [{"name":"tweets_19.txt"}]
+    for t in tweetName:
+        print t
+        req = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + t)
+        response = urllib2.urlopen(req)
+        obj = response.read()
+        objects = open(t, 'w')
+        objects.write(obj)
+        objects.close()
+        objects = open(t, 'r')
+        for line in objects:
+            try:
+                jL = json.loads(line)
+                tmp1 = jL["text"].lower()
+                tmp = tmp1.split()
+                retweet = jL["retweet_count"]
+                if retweet == 0:
+                    n += 1
+                    for i in pronoms.keys():
+                        if(i in tmp):
+                            pronoms[i] += 1
 			except:
-				pass
-		objects.close()
-	pronoms.update({"num_of_tweets": n})
-	return pronoms
-
+                            pass
+                            objects.close()
+                            pronoms.update({"num_of_tweets": n})
+                            return pronoms
+                            
 '''
 #Initate needed variables
     occur = [0,0,0,0,0,0,0,0,0]
